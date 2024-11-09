da25a8e56df267d5631d14d31212058500edf0ec
Mishulka <misailgromov@gmail.com>
2024-11-09 21:12:29 +0300

    make server, connection to DB, pages

--- server/index.js
+++ server/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
-
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+const port = 4000
 
-app.listen(4000,()=>{
-    console.log('server started on http://localhost:4000')
+
+
+app.get('/', (req, res) => res.send('Hello, World! This is your Express server.'));
+
+app.post('/', (req, res) => {
+   console.log(req.body)
+   res.json({ message: 'Data received successfully'})
 })
+
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
+
+mongoose.connect("mongodb://localhost:27017/jwt", {})
+.then(() => {
+    console.log("DB Connected successfully");
+})
+.catch(err=>{console.log(err.message);    
+});
+
+
+app.use(cors({
+    origin: [`https://localhost${port}`],
+    method: ['GET', 'POST'],
+    credentials: true,
+})
+);
+
+app.use(express.json());
